476179616f27479a9b78552c4b0fa641ed2f722d
Amirali Sheikh Hassani <amirali.sh3719@gmail.com>
2025-10-17 00:56:26 +0330

    add all functions

---
+++ HW1_40312358023.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+struct Student
+{
+    string name;
+    int studentID;
+    int units;
+    float gpa;
+    string securitycode;
+    Student* next;
+};
+
+void addStudent(student*& head, const string& name, int studentID, int units, float gpa, const string& securityCode);
+bool deleteStudent(Student*& head, int studentID);
+Student* searchStudent(Student* head, int studentID);
+bool updateStudent(Student* head, int studentID, int newUnits, float newGPA);
+void displayAllRecords(Student* head);
+
+
+int main() {
+    int choice;
+
+    string inputName;
+    int inputID, inputUnits;
+    float inputGPA;
+
+    while (true) {
+        cout << "\n";
+        cout << "================================================================================\n";
+        cout << "*                                                                              *\n";
+        cout << "*         Student Record System (Linked List Implementation)                    *\n";
+        cout << "*                                                                              *\n";
+        cout << "================================================================================\n";
+        cout << "*                                                                              *\n";
+        cout << "*  [1] Add New Student                                                          *\n";
+        cout << "*  [2] Delete a Student by StudentID                                            *\n";
+        cout << "*  [3] Find a Student by StudentID                                              *\n";
+        cout << "*  [4] Update a Student's GPA                                                   *\n";
+        cout << "*  [5] Display All Student Records                                              *\n";
+        cout << "*                                                                              *\n";
+        cout << "*  [0] Exit Program                                                            *\n";
+        cout << "*                                                                              *\n";
+        cout << "================================================================================\n";
+        cout << "\n";
+        cout << "  Enter your choice: ";
+
+        cin >> choice;
+
+        cin.ignore(); 
+
+        if (choice == 1) {
+            cout << "\n--- Add New Student ---\n";
+            cout << "Enter Student Name: ";
+            getline(cin, inputName); 
+            cout << "Enter Student ID: ";
+            cin >> inputID;
+            cout << "Enter Number of Units: ";
+            cin >> inputUnits;
+            cout << "Enter GPA: ";
+            cin >> inputGPA;
+            cout << "\n-> Student data received. (Record would be added here)\n";
+        } else if (choice == 2) {
+            cout << "\n--- Delete Student ---\n";
+            cout << "Enter Student ID to delete: ";
+            cin >> inputID;
+            cout << "\n-> Deletion request for ID " << inputID << " received. (Record would be deleted here)\n";
+        } else if (choice == 3) {
+            cout << "\n--- Search Student ---\n";
+            cout << "Enter Student ID to search: ";
+            cin >> inputID;
+            cout << "\n-> Searching for ID " << inputID << "... (Record would be displayed here)\n";
+        } else if (choice == 4) {
+            cout << "\n--- Update Student GPA ---\n";
+            cout << "Enter Student ID to update: ";
+            cin >> inputID;
+            cout << "Enter new GPA: ";
+            cin >> inputGPA;
+            cout << "\n-> Update request for ID " << inputID << " received. (GPA and security codes would be updated here)\n";
+        } else if (choice == 5) {
+            cout << "\n--- Displaying All Records ---\n";
+            cout << "-> (All student records would be printed here)\n";
+        } else if (choice == 0) {
+            cout << "\n================================================================================\n";
+            cout << "*                         Program Terminated. Goodbye!                         *\n";
+            cout << "================================================================================\n";
+            break;
+        } else {
+            cout << "\n!!! Invalid choice. Please enter a number from 0 to 5.\n";
+        }
+
+        cout << "\n\nPress Enter to return to the main menu...";
+        cin.get(); 
+    }
+
+    return 0;
+}
